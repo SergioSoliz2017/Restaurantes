26b65ec2d61e7c284f94e1aa4aff04153ab50afe
JoseMTJ-c1 <201801956@est.umss.edu>
2024-09-26 18:39:26 -0400

    Fragmento, item ofertas y nose q mas

--- app/src/main/java/com/example/restaurantes/Ofertas.kt
+++ app/src/main/java/com/example/restaurantes/Ofertas.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +18,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Ofertas : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ofertas, container, false)
-    }
+        // Inflamos el layout del fragmento
+        val view = inflater.inflate(R.layout.fragment_ofertas, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Ofertas.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Ofertas().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // Inicializamos el ListView
+        val listView: ListView = view.findViewById(R.id.lvLista)
+
+        // Creamos algunos datos de ejemplo
+        val ofertas = listOf(
+            Oferta("Oferta 1", "$50", R.drawable.banner, 4.5f),
+            Oferta("Oferta 2", "$75", R.drawable.banner, 4.0f),
+            Oferta("Oferta 3", "$100", R.drawable.banner, 5.0f)
+        )
+
+        // Configuramos el adaptador
+        val adapter = Adaptador_Ofertas(requireContext(), ofertas)
+        listView.adapter = adapter
+
+        return view
     }
 }
