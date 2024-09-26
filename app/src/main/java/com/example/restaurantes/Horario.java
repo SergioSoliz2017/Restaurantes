4a82df3a5f442f0167ee02082259e976583ffd64
Sergio <201801656@est.umss.edu>
2024-09-26 18:47:30 -0400

    Arreglo

---
+++ app/src/main/java/com/example/restaurantes/Horario.java
@@ -0,0 +1,12 @@
+package com.example.restaurantes;
+
+public class Horario {
+    String Abrir;
+    String Cerrar;
+    String Dia;
+    public Horario (String Dia,String Abrir,String Cerrar){
+        this.Abrir = Abrir;
+        this.Cerrar = Cerrar;
+        this.Dia= Dia;
+    }
+}
